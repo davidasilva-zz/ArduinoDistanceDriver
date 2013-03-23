f13e1cce53af1b21e58d0a68c77470511860ccbf
Dalmir <dalmir@debian>
2013-03-22 17:32:56 -0700

    sync

---
+++ Release/DistanceSensor/DistanceSensor.d
@@ -0,0 +1,4 @@
+DistanceSensor/DistanceSensor.d: ../DistanceSensor/DistanceSensor.cpp \
+ /storage/microcontroller/arduino/driver/sensor/distance/DistanceSensor/DistanceSensor.h
+
+/storage/microcontroller/arduino/driver/sensor/distance/DistanceSensor/DistanceSensor.h:
---
+++ Release/UltrasoundDistanceSensor/UltrasoundDistanceSensor.d
@@ -0,0 +1,38 @@
+UltrasoundDistanceSensor/UltrasoundDistanceSensor.d: \
+ ../UltrasoundDistanceSensor/UltrasoundDistanceSensor.cpp \
+ /storage/microcontroller/arduino/driver/sensor/distance/UltrasoundDistanceSensor/UltrasoundDistanceSensor.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/new.h \
+ /usr/share/arduino/hardware/arduino/variants/standard/pins_arduino.h \
+ /storage/microcontroller/arduino/driver/sensor/distance/DistanceSensor/DistanceSensor.h
+
+/storage/microcontroller/arduino/driver/sensor/distance/UltrasoundDistanceSensor/UltrasoundDistanceSensor.h:
+
+/usr/share/arduino/hardware/arduino/cores/arduino/Arduino.h:
+
+/usr/share/arduino/hardware/arduino/cores/arduino/binary.h:
+
+/usr/share/arduino/hardware/arduino/cores/arduino/WCharacter.h:
+
+/usr/share/arduino/hardware/arduino/cores/arduino/WString.h:
+
+/usr/share/arduino/hardware/arduino/cores/arduino/HardwareSerial.h:
+
+/usr/share/arduino/hardware/arduino/cores/arduino/Stream.h:
+
+/usr/share/arduino/hardware/arduino/cores/arduino/Print.h:
+
+/usr/share/arduino/hardware/arduino/cores/arduino/Printable.h:
+
+/usr/share/arduino/hardware/arduino/cores/arduino/new.h:
+
+/usr/share/arduino/hardware/arduino/variants/standard/pins_arduino.h:
+
+/storage/microcontroller/arduino/driver/sensor/distance/DistanceSensor/DistanceSensor.h:
